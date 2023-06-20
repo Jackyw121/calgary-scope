1187ed8485c0397fafc74ff9271cbe65c68038c6
Jacky Wong <jackw121@gmail.com>
2023-06-20 01:14:30 -0600

    mongodb connection done
    connected to database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const loginRoutes = require('./routes/login')
 
 //express app
@@ -14,14 +15,23 @@ app.use((req, res, next) =>{
     next()
 })
 
-//routes
+
 app.get('/', (req, res) => {
     res.json({msg: 'Welcome to the app'})
 })
 
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+.then(() =>{
+    // listen for requests
+app.listen(process.env.PORT, () => {
+    console.log('connected to db & listening on port ', process.env.PORT)
+    })
+})
+.catch((error) => {
+    console.log(error)
+})
+
+//routes
 app.use('/api/login', loginRoutes)
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port ', process.env.PORT)
-    })
