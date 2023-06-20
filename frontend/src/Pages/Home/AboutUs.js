fa51930e2ae4af83f4f37f4513da94063be75171
Jackyw121 <Jackw121@gmail.com>
2023-06-20 10:42:18 -0600

    small update on placeholder text
    very small change

--- frontend/src/Pages/Home/AboutUs.js
+++ frontend/src/Pages/Home/AboutUs.js
@@ -2,10 +2,7 @@ import React from 'react';
  
 function AboutUs (){
  return <address>
-            You can find us here:<br />
-            GeeksforGeeks<br />
-            5th & 6th Floor, Royal Kapsons, A- 118, <br />
-            Sector- 136, Noida, Uttar Pradesh (201305)
+            This is placeholder text until we can find a replacement. abceg
         </address>
 }
  
