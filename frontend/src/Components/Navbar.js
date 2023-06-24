d52a0f9cbfb7be68ce6a75d05f5d8ad72864b4b0
Jacky Wong <jackw121@gmail.com>
2023-06-24 15:27:48 -0600

    removed unneeded code
    got rid of unused code

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -34,24 +34,7 @@ const Navbar = () => {
   const handleClose = () => {
     setOpen(false);
   };
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
+  
   return (
     <AppBar position="sticky" color="solid">
       <Toolbar>
