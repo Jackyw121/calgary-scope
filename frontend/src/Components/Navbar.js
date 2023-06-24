1791a357beab1bdbcddb9296581569ba1b725f73
Jackyw121 <Jackw121@gmail.com>
2023-06-24 00:46:21 -0600

    update navbar.js
    very small change, transparent to solid

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -53,7 +53,7 @@ const Navbar = () => {
   };
 
   return (
-    <AppBar position="sticky" color="transparent">
+    <AppBar position="sticky" color="solid">
       <Toolbar>
       <Link to="">
       <IconButton>
