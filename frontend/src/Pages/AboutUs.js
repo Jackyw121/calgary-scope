b483cfb359645e1815b4910838003f68824b9f98
Jacky Wong <jackw121@gmail.com>
2023-06-26 22:42:04 -0600

    added about us info -oliver
    added abous info from oliver

--- frontend/src/Pages/AboutUs.js
+++ frontend/src/Pages/AboutUs.js
@@ -2,7 +2,23 @@ import React from 'react';
  
 function AboutUs (){
  return <address>
-            This is placeholder text until we can find a replacement. abceg
+<h1>About Us</h1>
+  <h2>Our Story</h2>
+  <p>SCOPE has supported individuals with developmental disabilities for over 30 years. Our core departments support children, youth, and families (Outreach), adults with mental health diagnoses (Community Support Team), and Seniors (Journeys). The Community Development department facilitates advocacy efforts (Disability Action Hall) and hosts the Picture This Film Festival.
+  Individuals served by SCOPE enjoy a flexible program and service that is customized according to their own preferences and needs. One size does not fit all, and every individual is entitled to pursue their dreams and goals on their terms. Person-centred planning is the cornerstone of SCOPE’s values and services.SCOPE is a leader in the community of disability services, specializing in creative ways of helping individuals live full lives in a community setting regardless of their level of need. Its services are frequently sought for a smaller proportion of individuals who have had repeated unsuccessful placements or chronic hospitalization.
+  We believe that the best services are based on a relationship; our role is to create services that work for the individual.</p>
+
+  <h2>Mission Statement</h2>
+  <p>"The Calgary SCOPE Society works in alliance with people with disabilities, their families and friends, and other community members to understand problems and create solutions to personal and social justice issues."</p>
+
+  <h2>Guiding Principles</h2>
+  <ul>
+    <li>Respect for diversity and choice</li>
+    <li>Valued roles and opportunities within community life</li>
+    <li>Careful listening, learning, and responsiveness</li>
+    <li>Positive, caring approaches, and relationships</li>
+    <li>Collective reflection, evaluation, and dialogue</li>
+  </ul>
         </address>
 }
  
