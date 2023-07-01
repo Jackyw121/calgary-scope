c775e833b5cb4f826a246c0b7956e54287e4a86a
Jacky Wong <jackw121@gmail.com>
2023-06-30 18:02:37 -0600

    created signup hook
    signup hook created and users can now sign up

--- frontend/src/Pages/Signup.js
+++ frontend/src/Pages/Signup.js
@@ -3,15 +3,17 @@ import Box from '@mui/material/Box';
 import Button from '@material-ui/core/Button';
 import TextField from '@mui/material/TextField';
 import { useClientContext } from '../Hooks/useClientContext';
+import { useSignup } from "../Hooks/useSignup";
 
 const Signup = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const {signup, error, isLoading} = useSignup()
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        console.log(email, password)
+        await signup(email, password)
     }
 
     return(
@@ -40,8 +42,9 @@ const Signup = () => {
               value={password}
             />
             <div className='buttonHolder'>
-                <Button type='submit' variant='contained' color = 'secondary'>Sign up</Button>
+                <Button disabled={isLoading} type='submit' variant='contained' color = 'secondary'>Sign up</Button>
             </div>
+            {error && <div className="error">{error}</div>}
             </form>
             </Box>
     )
