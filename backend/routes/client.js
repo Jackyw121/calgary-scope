6be8dcd0500a951a152247bc1e87bf48490be5ac
Jacky Wong <jackw121@gmail.com>
2023-07-01 00:11:06 -0600

    authorization
    users now need to be authenticated before viewing

--- backend/routes/client.js
+++ backend/routes/client.js
@@ -3,9 +3,14 @@ const {
     createClient, getClient, getClients, deleteClient, updateClient
 } = require('../controllers/clientController')
 
+const requireAuth = require('../middleware/requireAuth')
+
 const Client = require('../models/clientModel')
 
 const router = express.Router()
+
+// require auth for client information
+router.use(requireAuth)
 
 //GET all clients
 router.get('/', (getClients))
@@ -14,8 +19,7 @@ router.get('/', (getClients))
 router.get('/:id', (getClient))
 
 //POST a new client
-router.post('/', createClient
-)
+router.post('/', createClient)
 
 //DELETE a new client
 router.delete('/:id', deleteClient)
