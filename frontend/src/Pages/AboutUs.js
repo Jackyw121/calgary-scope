990e55c1d213077dda215e1b49e791ae0257b2bb
Jay Beladiya <122903647+Jaybeladiya99@users.noreply.github.com>
2023-07-04 12:05:41 -0600

    about us page added
    about us page added , footer needs to be added

--- frontend/src/Pages/AboutUs.js
+++ frontend/src/Pages/AboutUs.js
@@ -1,25 +1,45 @@
 import React from 'react';
- 
+
+import './AboutUs.css';
 function AboutUs (){
  return <address>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
 <h1>About Us</h1>
-  <h2>Our Story</h2>
-  <p>SCOPE has supported individuals with developmental disabilities for over 30 years. Our core departments support children, youth, and families (Outreach), adults with mental health diagnoses (Community Support Team), and Seniors (Journeys). The Community Development department facilitates advocacy efforts (Disability Action Hall) and hosts the Picture This Film Festival.
-  Individuals served by SCOPE enjoy a flexible program and service that is customized according to their own preferences and needs. One size does not fit all, and every individual is entitled to pursue their dreams and goals on their terms. Person-centred planning is the cornerstone of SCOPE’s values and services.SCOPE is a leader in the community of disability services, specializing in creative ways of helping individuals live full lives in a community setting regardless of their level of need. Its services are frequently sought for a smaller proportion of individuals who have had repeated unsuccessful placements or chronic hospitalization.
-  We believe that the best services are based on a relationship; our role is to create services that work for the individual.</p>
 
-  <h2>Mission Statement</h2>
-  <p>"The Calgary SCOPE Society works in alliance with people with disabilities, their families and friends, and other community members to understand problems and create solutions to personal and social justice issues."</p>
 
-  <h2>Guiding Principles</h2>
+<div class="color-container1">
+<h2 class="space">Our Story</h2>
+  <p></p>
+  <p class="container-text1">SCOPE has supported individuals with developmental disabilities for over 30 years. Our core departments support children, youth, and families (Outreach), adults with mental health diagnoses (Community Support Team), and Seniors (Journeys). The Community Development department facilitates advocacy efforts (Disability Action Hall) and hosts the Picture This Film Festival.   
+  </p>
+  <p class="container-text1">                                                                                                                                                                                 
+  Individuals served by SCOPE enjoy a flexible program and service that is customized according to their own preferences and needs. One size does not fit all, and every individual is entitled to pursue their dreams and goals on their terms. Person-centred planning is the cornerstone of SCOPE’s values and services.
+  </p> 
+  <p class="container-text1">  
+  SCOPE is a leader in the community of disability services, specializing in creative ways of helping individuals live full lives in a community setting regardless of their level of need. Its services are frequently sought for a smaller proportion of individuals who have had repeated unsuccessful placements or chronic hospitalization.
+  </p>
+  <p class="container-text1">  
+  We believe that the best services are based on a relationship; our role is to create services that work for the individual.
+  </p>
+  </div>
+  <div class="color-container2">
+  <h2 class="space">Mission Statement</h2>
+  <p class="container-text2">
+  "The Calgary SCOPE Society works in alliance with people with disabilities, their families and friends, and other community members to understand problems and create solutions to personal and social justice issues."</p>
+  </div>
+  <div class="color-container3">
+  <h2 class="space">Guiding Principles</h2>
   <ul>
-    <li>Respect for diversity and choice</li>
-    <li>Valued roles and opportunities within community life</li>
-    <li>Careful listening, learning, and responsiveness</li>
-    <li>Positive, caring approaches, and relationships</li>
-    <li>Collective reflection, evaluation, and dialogue</li>
+    <p class="container-text3">1. Respect for diversity and choice</p>
+    <p class="container-text3">2. Valued roles and opportunities within community life</p>
+    <p class="container-text3">3. Careful listening, learning, and responsiveness</p>
+    <p class="container-text3">4. Positive, caring approaches, and relationships</p>
+    <p class="container-text3">5. Collective reflection, evaluation, and dialogue</p>
   </ul>
+  </div>
+  
         </address>
 }
+
  
 export default AboutUs;
