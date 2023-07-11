70c2b148573b201e80eedbb6eba8f4716606a807
Jackyw121 <Jackw121@gmail.com>
2023-07-11 12:26:22 -0600

    Revert "TEST (REVERT IF BROKEN)"
    This reverts commit 22ea7700aea789a1b23ff0c0b051b157d8772883.

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -27,8 +27,7 @@ const userSchema = new Schema({
         type: String,
         required: true
     }
-},
-{timestamps: true})
+})
 
 //static signup method
 userSchema.statics.signup = async function (email, password, firstName, lastName) {
