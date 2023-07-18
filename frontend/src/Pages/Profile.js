1f1e4ca6757cbda60abdb592d51e56bb8563d633
Gelo <carldbenigno@gmail.com>
2023-07-18 16:45:02 -0600

    Form Submit feature

--- frontend/src/Pages/Profile.js
+++ frontend/src/Pages/Profile.js
@@ -18,6 +18,56 @@ function Profile (){
             <Button variant='contained'>Rabboni Form</Button>
             </Link>
             <AdminTest/>
+            <RabboniForm onAdd={addForm} />
+            {forms.map((formItem) => {
+              return <FormContainer
+                pFirstName={formItem.pFirstName}
+                pLastName={formItem.pLastName}
+                homeNumber={formItem.homeNumber}
+                cellPhoneNumber={formItem.cellPhoneNumber}
+                email={formItem.email}
+                streetAddress={formItem.streetAddress}
+                city={formItem.city}
+                province={formItem.province}
+                postalCode={formItem.postalCode}
+                childFirstName={formItem.childFirstName}
+                childLastName={formItem.childLastName}
+                childID={formItem.childID}
+                gender={formItem.gender}
+                birthDate={formItem.birthDate}
+                livesAtHome={formItem.livesAtHome}
+                typeOfService={formItem.typeOfService}
+                albertaHealthCardNumber={formItem.albertaHealthCardNumber}
+                diagnoses={formItem.diagnoses}
+                eating={formItem.eating}
+                dressing={formItem.dressing}
+                toileting={formItem.toileting}
+                activities={formItem.activities}
+                speechAndHearing={formItem.speechAndHearing}
+                vision={formItem.vision}
+                mobility={formItem.mobility}
+                specialInstructions={formItem.specialInstructions}
+                primaryDoctor={formItem.primaryDoctor}
+                clinic={formItem.clinic}
+                clinicAddress={formItem.clinicAddress}
+                clinicPhone={formItem.clinicPhone}
+                emergencyTransport={formItem.emergencyTransport}
+                emergencyOption={formItem.emergencyOption}
+                medications={formItem.medications}
+                dosage={formItem.dosage}
+                numOfTimes={formItem.numOfTimes}
+                prescribedFor={formItem.prescribedFor}
+                medications2={formItem.medications2}
+                dosage2={formItem.dosage2}
+                numOfTimes2={formItem.numOfTimes2}
+                prescribedFor2={formItem.prescribedFor2}
+                medications3={formItem.medications3}
+                dosage3={formItem.dosage3}
+                numOfTimes3={formItem.numOfTimes3}
+                prescribedFor3={formItem.prescribedFor3}
+                signature={formItem.signature}
+              />
+            })}
       </ThemeProvider>
     )
 }
