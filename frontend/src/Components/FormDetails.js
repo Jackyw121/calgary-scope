f3238f106dca14041272bcb27f9b19abefcf39e6
Jacky Wong <jackw121@gmail.com>
2023-08-05 15:36:28 -0600

    added rest of form details

--- frontend/src/Components/FormDetails.js
+++ frontend/src/Components/FormDetails.js
@@ -32,24 +32,44 @@ const FormDetails = ({ form }) => {
             <p><strong>Last Name: </strong>{form.pLastName}</p>
             <p><strong>Home Phone: </strong>{form.homeNumber}</p>
             <ToggleVisibility>
-            <p><strong>Cell Phone: </strong>{form.cellPhoneNumber}</p>
-            <p><strong>Email: </strong>{form.email}</p>
-            <p><strong>Street Address: </strong>{form.streetAddress}</p>
-            <p><strong>First Name: </strong>{form.pFirstName}</p>
-            <p><strong>Last Name: </strong>{form.pLastName}</p>
-            <p><strong>Home Phone: </strong>{form.homeNumber}</p>
-            <p><strong>First Name: </strong>{form.pFirstName}</p>
-            <p><strong>Last Name: </strong>{form.pLastName}</p>
-            <p><strong>Home Phone: </strong>{form.homeNumber}</p>
-            <p><strong>First Name: </strong>{form.pFirstName}</p>
-            <p><strong>Last Name: </strong>{form.pLastName}</p>
-            <p><strong>Home Phone: </strong>{form.homeNumber}</p>
-            <p><strong>First Name: </strong>{form.pFirstName}</p>
-            <p><strong>Last Name: </strong>{form.pLastName}</p>
-            <p><strong>Home Phone: </strong>{form.homeNumber}</p>
-            <p><strong>First Name: </strong>{form.pFirstName}</p>
-            <p><strong>Last Name: </strong>{form.pLastName}</p>
-            <p><strong>Home Phone: </strong>{form.homeNumber}</p>
+            <p><strong>City: </strong>{form.city}</p>
+            <p><strong>Province: </strong>{form.province}</p>
+            <p><strong>Postal Code: </strong>{form.postalcode}</p>
+            <p><strong>Child Fisrtname: </strong>{form.childFirstName}</p>
+            <p><strong>Child Lastname: </strong>{form.childLastName}</p>
+            <p><strong>Child ID: </strong>{form.childID}</p>
+            <p><strong>Gender: </strong>{form.gender}</p>
+            <p><strong>Birth Date: </strong>{form.birthDate}</p>
+            <p><strong>Lives at Home: </strong>{form.livesAtHome}</p>
+            <p><strong>Type Of Service: </strong>{form.typeOfService}</p>
+            <p><strong>Alberta Health Card Number: </strong>{form.albertaHealthCardNumber}</p>
+            <p><strong>Diagnoses: </strong>{form.diagnoses}</p>
+            <p><strong>Eating: </strong>{form.eating}</p>
+            <p><strong>Dressing: </strong>{form.dressing}</p>
+            <p><strong>Toileting: </strong>{form.toileting}</p>
+            <p><strong>Activities: </strong>{form.activities}</p>
+            <p><strong>Speech and Hearing: </strong>{form.speechAndHearing}</p>
+            <p><strong>Vision: </strong>{form.vision}</p>
+            <p><strong>Mobility: </strong>{form.mobility}</p>
+            <p><strong>Special Instructions: </strong>{form.specialInstructions}</p>
+            <p><strong>Primary Doctor: </strong>{form.primaryDoctor}</p>
+            <p><strong>Clinic: </strong>{form.clinic}</p>
+            <p><strong>Clinic Address: </strong>{form.clinicAddress}</p>
+            <p><strong>Clinic Phone: </strong>{form.clinicPhone}</p>
+            <p><strong>Emergency Transport: </strong>{form.emergencyTransport}</p>
+            <p><strong>Emergency Option: </strong>{form.emergencyOption}</p>
+            <p><strong>Medications: </strong>{form.medications}</p>
+            <p><strong>Dosage: </strong>{form.dosage}</p>
+            <p><strong>Number of Times : </strong>{form.numOfTimes}</p>
+            <p><strong>Prescribed For: </strong>{form.prescribedFor}</p>
+            <p><strong>Medications 2: </strong>{form.medications2}</p>
+            <p><strong>Dosage 2: </strong>{form.dosage2}</p>
+            <p><strong>Number of Times 2: </strong>{form.numOfTimes2}</p>
+            <p><strong>Prescribe For 2: </strong>{form.prescribedFor2}</p>
+            <p><strong>Medications 3: </strong>{form.medications3}</p>
+            <p><strong>Dosage 3: </strong>{form.dosage3}</p>
+            <p><strong>Number of Times 3: </strong>{form.numOfTimes3}</p>
+            <p><strong>Prescribe For 3: </strong>{form.prescribedFor3}</p>
             </ToggleVisibility>
             <span className="material-symbols-outlined" onClick={handleClick}>Delete</span>
         </div>
